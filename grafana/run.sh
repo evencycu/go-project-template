bdc85dad39e3bac8196f30877debf046bf081095
georgelee <georgelee@m800.com>
2020-07-31 16:56:17 +0800

    remove: only retain run grafana docker contianer script

--- grafana/run.sh
+++ grafana/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-VERSION="6.4.4"
+VERSION="7.1.1"
 
 docker run -ti --rm -p 3000:3000 --name grafana \
-  -v $PWD/etc/grafana:/etc/grafana \
-  -v $PWD/var:/var \
   grafana/grafana:$VERSION
